da445d93af09f9daf5512d39a7b99e913467cb56
Rounak858 <91734046+Rounak858@users.noreply.github.com>
2025-07-12 18:54:11 +0530

    server connect with frontend

--- server/cloudinary 2.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary');
-
-cloudinary.config({
-    cloud_name: 'dakvww5gn',
-    api_key: '789622916268367',
-    api_secret: 'hvNwCvy_V3fofQoS9f_xpyDAkb0'
-});
-
-module.exports = cloudinary
